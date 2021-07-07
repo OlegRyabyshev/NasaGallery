1ece11a05718a91bec92f771ad8f93a0caf3a4b2
Oleg Ryabyshev <oryabyshev@gmail.com>
2021-07-07 10:00:56 +0300

    dark theme

--- app/src/main/java/xyz/fcr/nasagallery/MainScreenFragment.kt
+++ app/src/main/java/xyz/fcr/nasagallery/MainScreenFragment.kt
@@ -1,13 +1,19 @@
 package xyz.fcr.nasagallery
 
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import xyz.fcr.nasagallery.databinding.FragmentMainScreenBinding
@@ -30,6 +36,9 @@ class MainScreenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setThemeFromSharedPreferences()
+
         setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
 
         binding.inputLayout.setEndIconOnClickListener {
@@ -48,9 +57,13 @@ class MainScreenFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.app_bar_fav -> toast("Favourite")
+
+            R.id.app_bar_settings -> showOptionsDialog()
+
             android.R.id.home -> {
                 activity?.let {
-                    BottomNavigationDrawerFragment().show(it.supportFragmentManager, "tag")
+                    BottomNavigationDrawerFragment()
+                        .show(it.supportFragmentManager, "tag")
                 }
             }
         }
@@ -92,6 +105,58 @@ class MainScreenFragment : Fragment() {
         Toast.makeText(context, string, Toast.LENGTH_SHORT).apply {
             setGravity(Gravity.BOTTOM, 0, 250)
             show()
+        }
+    }
+
+    private fun showOptionsDialog() {
+
+        val themes = arrayOf(
+            resources.getString(R.string.action_theme_light),
+            resources.getString(R.string.action_theme_dark)
+        )
+
+        val builder = AlertDialog.Builder(requireContext())
+
+        builder.setTitle(resources.getString(R.string.action_theme))
+
+        val sharedPreferences: SharedPreferences = requireContext()
+            .getSharedPreferences("MyUserPref", Context.MODE_PRIVATE)
+
+        val editor = sharedPreferences.edit()
+
+        val themeInt: Int = getCurrentTheme()
+        builder.setSingleChoiceItems(themes, themeInt) { dialog: DialogInterface?, which: Int ->
+            if (which == 0) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putString("calculator_theme", "light")
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putString("calculator_theme", "dark")
+            }
+            editor.apply()
+        }
+
+        builder.show()
+    }
+
+    private fun getCurrentTheme(): Int {
+        return when ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_NO -> 0
+            Configuration.UI_MODE_NIGHT_YES -> 1
+            else -> -1
+        }
+    }
+
+    private fun setThemeFromSharedPreferences() {
+        val sharedPreferences: SharedPreferences = requireContext()
+            .getSharedPreferences("MyUserPref", Context.MODE_PRIVATE)
+
+        val savedTheme = sharedPreferences.getString("calculator_theme", "")
+
+        if (savedTheme == "light") {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else if (savedTheme == "dark") {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
     }
 
